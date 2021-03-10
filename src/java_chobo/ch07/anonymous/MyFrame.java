0d95d4a052eb273e4eaa6311d94e72289adbcb22
mywns123 <wnsduq2000@naver.com>
2021-03-10 15:49:29 +0900

    수정

--- src/java_chobo/ch07/anonymous/MyFrame.java
+++ src/java_chobo/ch07/anonymous/MyFrame.java
@@ -11,6 +11,7 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
+@SuppressWarnings("serial")
 public class MyFrame extends JFrame {
 
 	private JPanel contentPane;
